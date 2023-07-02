5602253ca3e4d5be8429fcc09e09a0cff43dd02a
preciousimo <preciousimoniakemu@gmail.com>
2023-07-02 17:33:40 +0100

    🔍 User Search: Enable search by account number/ID

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from . import views
+from main import views, transfer
 
 urlpatterns = [
-	path('', views.index, name="index"),
+    path("", views.index, name="index"),
+
+    # Transfers
+    path("search-account/", transfer.search_users_account_number, name="search-account"),
 ]
