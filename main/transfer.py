fdae991c3cbe47dc88d918b14a941537dab97f2d
preciousimo <preciousimoniakemu@gmail.com>
2023-07-02 19:56:30 +0100

    💸 Transfer Payment Confirmation

--- main/transfer.py
+++ main/transfer.py
@@ -74,6 +74,16 @@ def AmountTransferProcess(request, account_number):
     else:
         messages.warning(request, "Error Occured, Try again later.")
         return redirect("account:account")
-
-
-
+    
+def TransferConfirmation(request, account_number, transaction_id):
+    try:
+        account = Account.objects.get(account_number=account_number)
+        transaction = Transaction.objects.get(transaction_id=transaction_id)
+    except:
+        messages.warning(request, "Transaction does not exist.")
+        return redirect("account:account")
+    context = {
+        "account":account,
+        "transaction":transaction
+    }
+    return render(request, "transfer/transfer_confirmation.html", context)
--- main/urls.py
+++ main/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("search-account/", transfer.search_users_account_number, name="search-account"),
     path("amount-transfer/<account_number>/", transfer.AmountTransfer, name="amount-transfer"),
     path("amount-transfer-process/<account_number>/", transfer.AmountTransferProcess, name="amount-transfer-process"),
+    path("transfer-confirmation/<account_number>/<transaction_id>/", transfer.TransferConfirmation, name="transfer-confirmation"),
 ]
