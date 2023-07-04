45a06690f367673ebd5c291b6be59ec9467dad50
preciousimo <preciousimoniakemu@gmail.com>
2023-07-04 11:35:41 +0100

    ⚙️ Process Request Amount: Implement process logic

--- main/payment_request.py
+++ main/payment_request.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from account.models import Account
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
+from django.contrib import messages
+from decimal import Decimal
+from main.models import Transaction
 
 @login_required
 def SearchUsersRequest(request):
@@ -26,3 +29,38 @@ def AmountRequest(request, account_number):
         "account": account,
     }
     return render(request, "payment_request/amount_request.html", context)
+
+def AmountRequestProcess(request, account_number):
+    account = Account.objects.get(account_number=account_number)
+
+    sender = request.user
+    reciever = account.user
+
+    sender_account = request.user.account
+    reciever_account = account
+
+    if request.method == "POST":
+        amount = request.POST.get("amount-request")
+        description = request.POST.get("description")
+
+        new_request = Transaction.objects.create(
+            user=request.user,
+            amount=amount,
+            description=description,
+
+            sender=sender,
+            reciever=reciever,
+
+            sender_account=sender_account,
+            reciever_account=reciever_account,
+
+            status="request_processing",
+            transaction_type="request"
+        )
+        new_request.save()
+        transaction_id = new_request.transaction_id
+        return redirect("main:amount-request-confirmation", account.account_number, transaction_id)
+    else:
+        messages.warning(request, "Error Occured, try again later.")
+        return redirect("account:dashboard")
+
