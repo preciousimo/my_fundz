8e8fe7b66c223d1aed45e5d18bbea9afb8f34136
preciousimo <preciousimoniakemu@gmail.com>
2023-07-05 14:53:34 +0100

    💸 Withdraw Cash

--- main/credit_card.py
+++ main/credit_card.py
@@ -36,4 +36,29 @@ def fund_credit_card(request, card_id):
         else:
             messages.warning(request, "Insufficient Funds")
             return redirect("main:card-detail", credit_card.card_id)
+
+@login_required
+def withdraw_fund(request, card_id):
+    account = Account.objects.get(user=request.user)
+    credit_card = CreditCard.objects.get(card_id=card_id, user=request.user)
+
+    if request.method == "POST":
+        amount = request.POST.get("amount")
+        print(amount)
+
+        if credit_card.amount >= Decimal(amount) and credit_card.amount != 0.00:
+            account.account_balance += Decimal(amount)
+            account.save()
+
+            credit_card.amount -= Decimal(amount)
+            credit_card.save()
+
+            messages.success(request, "Withdrawal Successfull")
+            return redirect("main:card-detail", credit_card.card_id)
+        elif credit_card.amount == 0.00:
+            messages.warning(request, "Insufficient Funds")
+            return redirect("main:card-detail", credit_card.card_id)
+        else:
+            messages.warning(request, "Insufficient Funds")
+            return redirect("main:card-detail", credit_card.card_id)
             
