1bd29f35b4f749d2d4124b2325c40c6df15f9ea4
preciousimo <preciousimoniakemu@gmail.com>
2023-07-05 12:35:17 +0100

    🔒 Create Credit Card Models: Define card models

--- main/admin.py
+++ main/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
-from main.models import Transaction
+from main.models import Transaction, CreditCard
 
 class TransactionAdmin(admin.ModelAdmin):
     list_editable = ['amount', 'status', 'transaction_type']
     list_display = ['user', 'amount', 'status', 'transaction_type', 'reciever', 'sender']
 
 
+class CreditCardAdmin(admin.ModelAdmin):
+    list_editable = ['amount', 'card_type']
+    list_display = ['user', 'amount', 'card_type']
+    
+
 admin.site.register(Transaction, TransactionAdmin)
+admin.site.register(CreditCard, CreditCardAdmin)
